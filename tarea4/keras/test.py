52ac858057848898883c2fe2c89069240182a236
lnhrt2806 <jimena.salas.alfaro@gmail.com>
2019-10-06 22:50:18 -0600

    keras prediction, some results, changes to plot confusion matrix

---
+++ tarea4/keras/test.py
@@ -0,0 +1,41 @@
+import numpy as np
+import mnist
+
+from keras.models import Sequential
+from keras.layers import Dense
+from sklearn.metrics import classification_report, confusion_matrix
+from mnist_helpers import *
+
+#Load test data
+test_images = mnist.test_images()
+test_labels = mnist.test_labels()
+
+#Normalize images from [-0.5, 0.5]
+test_images = (test_images / 255) - 0.5
+
+#Flatten images to a single vector of 784
+test_images = test_images.reshape((-1, 784))
+
+#Model building
+model = Sequential([
+  #Input shape for network
+  Dense(64, activation='relu', input_shape=(784,)),
+  Dense(64, activation='relu'),
+  Dense(10, activation='softmax')
+  ])
+
+#Model loading
+model.load_weights('Models/model.h5')
+
+#Predict
+predictions = model.predict(test_images)
+#Vector with winner class for each image
+max_predictions = np.argmax(predictions, axis=1)
+
+matrix = confusion_matrix(test_labels, max_predictions)
+report = classification_report(test_labels, max_predictions)
+
+print("CONFUSION MATRIX\n" + str(matrix))
+print("\nCLASSIFICATION REPORT\n" + str(report))
+
+plot_confusion_matrix(matrix)
---
+++ tarea4/keras/train.py
@@ -0,0 +1,68 @@
+#Example from https://victorzhou.com/blog/keras-neural-network-tutorial/
+
+import numpy as np 
+import mnist
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import to_categorical
+from keras.optimizers import Adam
+
+#Load train and test data
+
+train_images = mnist.train_images()
+train_labels = mnist.train_labels()
+test_images = mnist.test_images()
+test_labels = mnist.test_labels()
+
+#Normalize images from [-0.5, 0.5]
+train_images = (train_images / 255) - 0.5
+test_images = (test_images / 255) - 0.5
+
+#Flatten images to a single vector of 784
+train_images = train_images.reshape((-1, 784))
+test_images = test_images.reshape((-1, 784))
+
+
+
+#Model building
+model = Sequential([
+  #Input shape for network
+  Dense(64, activation='relu', input_shape=(784,)),
+  Dense(64, activation='relu'),
+  Dense(10, activation='softmax')
+  ])
+
+
+#Model compiling
+model.compile(
+  #gradient optimizer
+  optimizer='adam',
+  #loss function for softmax output layer
+  loss='categorical_crossentropy',
+  #metrics for classification problem
+  metrics=['accuracy'])
+
+
+#Model training
+model.fit(
+  #X data
+  train_images,
+  #Y data encoded as canonic vector
+  to_categorical(train_labels),
+  #Iterations for the whole data set
+  epochs=100,
+  #Samples per gradient update
+  batch_size=64,
+  #Validation fraction from training data
+  validation_split=0.3)
+
+#Model testing
+loss, accuracy = model.evaluate(
+                  test_images,
+                  to_categorical(test_labels))
+
+print("Loss: " + str(loss) + "\nAccuracy: " + str(accuracy))
+
+#Model saving
+model.save_weights('Models/model.h5')
